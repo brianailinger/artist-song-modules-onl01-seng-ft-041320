d5b9bfbad33872acacd944c51dd6d17b8d9da0bb
Brian Ailinger <brianjr48@yahoo.com>
2020-04-30 19:39:45 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+  @@artists = []
+
+  def initialize
+    @@artists << self
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
